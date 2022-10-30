bf02d83d1753394b665164984ecacd677e41be6c
ilona <gogiashvili.ilona@gmail.com>
2022-10-29 21:38:19 -0400

    commit tree 39e65d42a0e81b8eda09770a1d420cb2935350f8

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(jsonData => renderBooks(jsonData));
 }
 
 function renderBooks(books) {
@@ -12,6 +15,40 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+
+document.addEventListener('DOMContentLoaded', function () {
+  fethchBooks();
+})
+// function fetchBooks() {
+//   // To pass the tests, don't forget to return your fetch!
+//   return fetch("https://anapioficeandfire.com/api/books")
+//     .then((resp) => resp.json())
+//     .then((jsonData) => renderBooks(jsonData));
+//   console.log('before fetch returns')
+// }
+// function renderBooks(books) {
+//   const main = document.querySelector('main');
+//   books.forEach(book => {
+//     const h2 = document.createElement('h2');
+//     h2.innerHTML = book.name;
+//     main.appendChild(h2);
+//   });
+// }
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   fetchBooks();
+//   console.log("after fetchBooks");
+// })
+
+
+
+
+/*
+
+function renderBooks(books) {
+  books.forEach(book => {
+  renderBook(book)
+  });
+  }
+
+  */
